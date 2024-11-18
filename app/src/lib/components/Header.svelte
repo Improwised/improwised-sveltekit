9787563735e09fa19313d631e2aae329931504a3
gorkhiyamonika <gorkhiyamonika@gmail.com>
2024-11-18 14:45:45 +0530

    feat: New Home page added v1

---
+++ app/src/lib/components/Header.svelte
@@ -0,0 +1,189 @@
+<header
+	class="astronav-sticky-header sticky top-0 z-20 border-b border-transparent py-5 transition-all"
+	data-astro-cid-eo7fz7pr=""
+>
+	<div class="mx-auto max-w-screen-xl px-5">
+		<div
+			class="relative z-10 flex flex-col items-center justify-between lg:flex-row"
+			data-astro-transition-scope="astro-o7bz76pi-1"
+		>
+			<div class="flex w-full items-center justify-between lg:w-auto">
+				<a href="/" class="-ml-2 flex items-center px-2 transition"
+					><img src="images/logo.png" class="logo" alt="logo" />
+				</a>
+				<div class="block lg:hidden">
+					<button id="astronav-menu" aria-label="Toggle Menu">
+						<svg
+							fill="currentColor"
+							class="h-4 w-4 text-gray-800"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							xmlns="https://www.w3.org/2000/svg"
+						>
+							<title>Toggle Menu</title>
+							<path
+								class="astronav-close-icon astronav-toggle hidden"
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+								d="M18.278 16.864a1 1 0 01-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 01-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 011.414-1.414l4.829 4.828 4.828-4.828a1 1 0 111.414 1.414l-4.828 4.829 4.828 4.828z"
+							>
+							</path>
+							<path
+								class="astronav-open-icon astronav-toggle"
+								fill-rule="evenodd"
+								d="M4 5h16a1 1 0 010 2H4a1 1 0 110-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2z"
+							>
+							</path>
+						</svg>
+					</button>
+				</div>
+			</div>
+			<nav class="astronav-items astronav-toggle  mt-2 hidden w-full lg:mt-0 lg:flex lg:w-auto">
+				<ul class="flex flex-col lg:flex-row lg:gap-3">
+					<li>
+						<a
+							href="/"
+							class="focus-visible:shadow-outline-indigo flex  rounded-full py-2 text-md text-slate-bold transition hover:text-theme-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 lg:px-3"
+						>
+							Home
+						</a>
+					</li>
+					<li>
+						<a
+							href="/"
+							class="focus-visible:shadow-outline-indigo flex rounded-full py-2 text-md text-slate-bold transition hover:text-theme-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 lg:px-3"
+						>
+							About Us
+						</a>
+					</li>
+					<li class="relative isolate z-50">
+						<!-- svelte-ignore a11y_role_supports_aria_props_implicit -->
+						<menu class="astronav-dropdown group" aria-expanded="false">
+							<button
+								class="focus-visible:shadow-outline-indigo flex w-full items-center gap-1 rounded-full py-2 text-md text-slate-boldtransition hover:text-slate-bold focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 lg:w-auto lg:px-3"
+							>
+								<span>Services</span>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="3"
+									stroke="currentColor"
+									class="mt-0.5 h-3 w-3 group-open:rotate-180"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+									></path>
+								</svg>
+							</button>
+							<div class="astronav-dropdown dropdown-toggle hidden" aria-expanded="false">
+								<div
+									class="z-50 w-full origin-top-left border-l border-slate-200 lg:absolute lg:left-0 lg:w-48 lg:border-0"
+								>
+									<div
+										class="flex flex-col px-3 lg:rounded-md lg:border lg:bg-white lg:py-2 lg:shadow"
+									>
+										<a
+											href="/"
+											class="focus-visible:shadow-outline-indigo rounded py-1 text-md text-slate-bold hover:text-theme-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
+										>
+
+										Platform Engineering
+
+										</a><a
+											href="/"
+											class="focus-visible:shadow-outline-indigo rounded py-1 text-md text-slate-bold hover:text-theme-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
+										>
+
+										Cloud Infrastructure Services
+
+										</a><a
+											href="/"
+											class="focus-visible:shadow-outline-indigo rounded py-1 text-md text-slate-bold hover:text-theme-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
+										>
+
+										Product Modernization
+
+										</a><a
+											href="/"
+											class="focus-visible:shadow-outline-indigo rounded py-1 text-md text-slate-bold hover:text-theme-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
+										>
+
+                                Technology Consulting
+
+										</a>
+									</div>
+								</div>
+							</div>
+						</menu>
+					</li>
+
+					<li>
+						<a
+							href="/"
+							class="focus-visible:shadow-outline-indigo flex rounded-full py-2 text-md text-slate-bold transition hover:text-theme-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 lg:px-3"
+						>
+							Case Studies
+						</a>
+					</li>
+					<li>
+						<a
+							href="/"
+							class="focus-visible:shadow-outline-indigo flex rounded-full py-2 text-md text-slate-bold transition hover:text-theme-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 lg:px-3"
+						>
+							Careers
+						</a>
+					</li>
+					<li>
+						<a
+							href="/"
+							class="focus-visible:shadow-outline-indigo flex rounded-full py-2 text-md text-slate-bold transition hover:text-theme-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 lg:px-3"
+						>
+							Blogs
+						</a>
+					</li>
+					<!-- <li>
+						<a
+							href="/"
+							class="focus-visible:shadow-outline-indigo flex rounded-full py-2 text-md text-slate-bold transition hover:text-theme-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 lg:px-3"
+						>
+							Contact Us
+						</a>
+					</li> -->
+				</ul>
+				<div class="mt-3 flex items-center gap-4 lg:hidden">
+					<a
+						href="/"
+						class="focus-visible:shadow-outline-indigo bg-grd-primary-600 w-full rounded-full px-5 py-2 text-center text-white transition hover:bg-indigo-800 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
+						>Contact Us
+					</a>
+				</div>
+			</nav>
+			<div>
+				<div class="hidden items-center gap-4 lg:flex">
+					<a
+						href="/"
+						class="focus-visible:shadow-outline-indigo bg-grd-primary-600 group inline-flex items-center gap-px rounded-full px-4 py-2 text-center text-md text-white transition hover:bg-indigo-800 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2"
+					>
+						<span>Contact Us</span>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="mt-px h-4 w-4 transition-transform group-hover:translate-x-1"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+								clip-rule="evenodd"
+							></path>
+						</svg>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script>
+	import Header from "$lib/components/Header.svelte";
+
+	import Footer from "$lib/components/Footer.svelte";
+	import "../app.css";
+
+
+</script>
+
+<svelte:head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="icon" type="image/svg+xml" href="favicon.svg" />
+	<meta name="generator" content="Improwised" />
+	<title>Improwised Technology</title>
+	<link rel="canonical" href="https://www.improwised.com/" />
+	<meta name="description" content="Let us help you with cloud infrastructure and SAAS software development services to create revenue channels. Our technical support, IT Managed Services will let you focus on your strength while we are managing your software and IT assets." />
+	<meta name="robots" content="index, follow" />
+	<meta property="og:title" content="Improwised" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="opengraph.jpg" />
+	<meta property="og:url" content="https://www.improwised.com/" />
+	<meta property="og:image:url" content="opengraph.jpg" />
+	<meta property="og:image:alt" content="Improwised" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@improwised" />
+	<meta
+		name="twitter:creator"
+		content="@improwised"
+	/>
+
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+
+
+</svelte:head>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<Footer />
+	<script src="js/custom.js"></script>
+</div>
